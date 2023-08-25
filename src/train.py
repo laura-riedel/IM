1377b5aab219010459814ce56d506354ec17315c
laura-riedel <laura.riedel@posteo.de>
2023-08-25 09:21:36 +0200

    Move model init higher; change parameter exception value (5Mio-> 250,000)

--- src/train.py
+++ src/train.py
@@ -30,29 +30,7 @@ with wandb.init() as run: # project name set through yaml
 
     # make sure only one CPU thread is used
     torch.set_num_threads(1)
-
-    # initialise logger
-    wandb_logger = WandbLogger(log_model=False)
-
-    # initialise callbacks
-    # checkpoint = utils.wandb_checkpoint_init()
-    early_stopping = utils.earlystopping_init(patience=config.patience)
-
-    # initialise trainer
-    trainer = pl.Trainer(accelerator='cpu', 
-                        logger=wandb_logger,
-                        log_every_n_steps=config.log_steps,
-                        max_epochs=config.max_epochs,
-                        callbacks=[early_stopping], # ,checkpoint
-                        enable_checkpointing=False,
-                        deterministic=True,
-                        max_time='00:03:00:00') # stop after 3h
-
-    # initialise DataModule
-    datamodule = ukbb_data.UKBBDataModule(data_path='/ritter/share/data/UKBB/ukb_data/',
-                                        ica=config.ica,
-                                        good_components=config.good_components) 
-
+    
     # initialise model
     # catch cases where the model architecture is impossible
     # or would cause memory issues
@@ -77,21 +55,41 @@ with wandb.init() as run: # project name set through yaml
         # finish run 
         wandb.finish(exit_code=555)
         sys.exit()
-        
+    
     # catch models with too many parameters
     pytorch_total_params = sum(torch.numel(p) for p in variable_CNN.parameters())
     # threshold: 5Mio
-    if pytorch_total_params > 5000000:
+    if pytorch_total_params > 250000:
         # add tag for later filtering
         run.tags = run.tags + ('too_many_params',) 
         # finish run 
         wandb.finish(exit_code=555)
         sys.exit()
 
-    # train model
-    trainer.fit(variable_CNN, datamodule=datamodule)
+    # initialise logger
+    wandb_logger = WandbLogger(log_model=False)
 
-    # test model
-    # trainer.test(ckpt_path='best', datamodule=datamodule)  
+    # initialise callbacks
+    # checkpoint = utils.wandb_checkpoint_init()
+    early_stopping = utils.earlystopping_init(patience=config.patience)
+
+    # initialise trainer
+    trainer = pl.Trainer(accelerator='cpu', 
+                        logger=wandb_logger,
+                        log_every_n_steps=config.log_steps,
+                        max_epochs=config.max_epochs,
+                        callbacks=[early_stopping], # ,checkpoint
+                        enable_checkpointing=False,
+                        deterministic=True,
+                        max_time='00:03:00:00') # stop after 3h
+
+    # initialise DataModule
+    datamodule = ukbb_data.UKBBDataModule(data_path='/ritter/share/data/UKBB/ukb_data/',
+                                        ica=config.ica,
+                                        good_components=config.good_components) 
+        
+
+    # train model
+    trainer.fit(variable_CNN, datamodule=datamodule)  
     
     wandb.finish()
